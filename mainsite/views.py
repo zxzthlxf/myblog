65c89f9054a21c87539bcad5b561e99fac596aae
zxzthlxf <zxzthlxf@163.com>
2019-08-04 00:54:11 +0800

    2.2 finish

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count))+str(post)+"<hr>")
+        post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+    return HttpResponse(post_lists)
+
